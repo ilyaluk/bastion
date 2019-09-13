da54defe1605b1e7743ac17672f9f50ccb85b6ff
Ilya Lukyanov <ilya@luk.moe>
2019-09-13 18:03:01 +0300

    Support inetd-style connection

--- internal/child/child.go
+++ internal/child/child.go
@@ -3,12 +3,62 @@ package child
 import (
 	"net"
 	"os"
+	"time"
 
 	"github.com/ilyaluk/bastion/internal/config"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
+
+type nilTCPAddr string
+
+func (n nilTCPAddr) Network() string {
+	return "tcp"
+}
+
+func (n nilTCPAddr) String() string {
+	return string(n)
+}
+
+type stdioNetConn struct{}
+
+func (s stdioNetConn) Read(b []byte) (n int, err error) {
+	return os.Stdin.Read(b)
+}
+
+func (s stdioNetConn) Write(b []byte) (n int, err error) {
+	return os.Stdout.Write(b)
+}
+
+func (s stdioNetConn) Close() error {
+	err1 := os.Stdin.Close()
+	err2 := os.Stdout.Close()
+	if err1 != nil || err2 != nil {
+		return errors.Errorf("error closing in: %v, out: %", err1, err2)
+	}
+	return nil
+}
+
+func (s stdioNetConn) LocalAddr() net.Addr {
+	return nilTCPAddr("stdin")
+}
+
+func (s stdioNetConn) RemoteAddr() net.Addr {
+	return nilTCPAddr("stdout")
+}
+
+func (s stdioNetConn) SetDeadline(t time.Time) error {
+	return nil
+}
+
+func (s stdioNetConn) SetReadDeadline(t time.Time) error {
+	return nil
+}
+
+func (s stdioNetConn) SetWriteDeadline(t time.Time) error {
+	return nil
+}
 
 func Run() (err error) {
 	logconfig := zap.NewDevelopmentConfig()
@@ -24,21 +74,30 @@ func Run() (err error) {
 		log.Fatalf("usage: %s config.yaml", os.Args[0])
 	}
 
+	inetdStyle := false
+	if len(os.Args) >= 3 && os.Args[2] == "-i" {
+		inetdStyle = true
+	}
+
 	conf, err := config.Read(os.Args[1])
 	if err != nil {
 		return
 	}
 	log.Infow("loaded config", "conf", conf.Child)
 
-	// create client connection from fd
-	nConn, err := net.FileConn(os.NewFile(3, "nConn"))
-	for err != nil {
-		return errors.Wrap(err, "failed to create client conn")
+	var clientConn net.Conn
+	if inetdStyle {
+		clientConn = stdioNetConn{}
+	} else {
+		clientConn, err = net.FileConn(os.NewFile(3, "nConn"))
+		for err != nil {
+			return errors.Wrap(err, "failed to create client conn")
+		}
+		defer clientConn.Close()
 	}
-	defer nConn.Close()
 
 	server := NewServer(conf.Child, log)
-	if err = server.ProcessConnection(nConn); err != nil {
+	if err = server.ProcessConnection(clientConn); err != nil {
 		log.Errorw("error while handling connection", "err", err)
 		return
 	}
